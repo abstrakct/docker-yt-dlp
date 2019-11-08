156ac026fdb6efd12b18d46073e2718b92fbabac
Tim Linden <tim@timlinden.com>
2019-11-07 22:10:52 -0500

    Update run-youtube-dl.sh
    Trying to remove windows returns :(

--- run-youtube-dl.sh
+++ run-youtube-dl.sh
@@ -1,1 +1,6 @@
 #!/bin/sh
+filename='channels.txt'
+echo Start
+while read p; do 
+    youtube-dl -f best -ciw -o "/download/%(channel_id)s/%(title)s.%(ext)s" -v https://www.youtube.com/channel/$p
+done < $filename
